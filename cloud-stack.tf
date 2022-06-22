8f178fa8b4763c43e1c5949dcc2069823f548afb
Ishan Jain <51803183+ishanjainn@users.noreply.github.com>
2022-06-22 12:15:53 -0400

    Update cloud-stack.tf

--- cloud-stack.tf
+++ cloud-stack.tf
@@ -17,8 +17,8 @@ provider "grafana" {
 resource "grafana_cloud_stack" "ishanterraformde" {
   provider = grafana.first
 
-  name        = "ishanjterraformde"
-  slug        = "ishanjterraformde"
+  name        = "ishanjterraformdem"
+  slug        = "ishanjterraformdem"
   region_slug = "us" # Example “us”,”eu” etc
 }
 
