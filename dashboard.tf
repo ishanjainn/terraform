b8904f2e7e86eb9b8f42bfa6f552c8d1bb3e675d
Ishan Jain <ishan.jain0099@gmail.com>
2022-06-22 10:36:54 -0400

    adding terraform files

--- dashboard.tf
+++ dashboard.tf
@@ -5,3 +5,9 @@ resource "grafana_dashboard" "influxdb" {
   config_json = file("dashboards/sample-dashboard.json")
   folder      = grafana_folder.Demos.id
 }
+resource "grafana_dashboard" "aws" {
+  provider = grafana.second
+
+  config_json = file("dashboards/test-dashboard.json")
+  folder      = grafana_folder.Demos.id
+}
