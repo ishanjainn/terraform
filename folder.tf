a6afd5a7fa0ece3c06aa2d2cea3765c767a9535b
Ishan Jain <51803183+ishanjainn@users.noreply.github.com>
2022-06-22 14:24:47 -0400

    Update folder.tf

--- folder.tf
+++ folder.tf
@@ -4,3 +4,10 @@ resource "grafana_folder" "Demos" {
   uid   = "12345"
   title = "Demos"
 }
+
+resource "grafana_folder" "Demos-2" {
+  provider = grafana.second
+
+  uid   = "123456"
+  title = "Demos-2"
+}
