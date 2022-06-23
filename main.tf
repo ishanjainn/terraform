fb2f5103a221fd6dd2f14e1c43bd2ef05d15a81c
Ishan Jain <ishan.jain0099@gmail.com>
2022-06-23 14:32:44 -0400

    adding terraform files

--- main.tf
+++ main.tf
@@ -1,15 +1,15 @@
 terraform {
- required_providers {
-   grafana = {
-     source  = "grafana/grafana"
-     version = "1.24.0"
-   }
- }
+  required_providers {
+    grafana = {
+      source  = "grafana/grafana"
+      version = "1.24.0"
+    }
+  }
 }
 
 provider "grafana" {
-  alias = "second" 
-  
-  url   = "https://ishanawsterraformdem.grafana.net/"
-  auth  = "eyJrIjoiMG9VS2lzVGRsZVBkRW8xQlB2aDlrZnVycE80WTJNY0MiLCJuIjoieW8iLCJpZCI6MX0="
+  alias = "second"
+
+  url  = "https://ishanawsterraformdem.grafana.net/"
+  auth = "eyJrIjoiMG9VS2lzVGRsZVBkRW8xQlB2aDlrZnVycE80WTJNY0MiLCJuIjoieW8iLCJpZCI6MX0="
 }
