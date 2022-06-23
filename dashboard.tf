1fe0e431aef673c27938080624e167d30e8fa473
Ishan Jain <51803183+ishanjainn@users.noreply.github.com>
2022-06-23 14:22:26 -0400

    Delete dashboard.tf

--- dashboard.tf
+++
@@ -1,8 +0,0 @@
-# Using a JSON file
-resource "grafana_dashboard" "influxdb" {
-  provider = grafana.second
-
-  for_each    = fileset("${path.module}/dashboards", "*.json")
-  config_json = file("${path.module}/dashboards/${each.key}")
-  folder      = grafana_folder.Demos.id
-}
