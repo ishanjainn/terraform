52f57a0976e9faef10f0479cf733c7164f27428d
Ishan Jain <ishan.jain0099@gmail.com>
2022-06-23 14:34:20 -0400

    adding terraform files

--- dashboards.tf
+++ dashboards.tf
@@ -1,16 +1,22 @@
 resource "grafana_dashboard" "production" {
+  provider = grafana.second
+
   for_each    = fileset("${path.module}/dashboards/production", "*.json")
   config_json = file("${path.module}/dashboards/production/${each.key}")
   folder      = grafana_folder.Production.id
 }
 
 resource "grafana_dashboard" "staging" {
+  provider = grafana.second
+
   for_each    = fileset("${path.module}/dashboards/staging", "*.json")
   config_json = file("${path.module}/dashboards/staging/${each.key}")
   folder      = grafana_folder.Staging.id
 }
 
 resource "grafana_dashboard" "dev" {
+  provider = grafana.second
+
   for_each    = fileset("${path.module}/dashboards/dev", "*.json")
   config_json = file("${path.module}/dashboards/dev/${each.key}")
   folder      = grafana_folder.Dev.id
--- folder.tf
+++ folder.tf
@@ -1,11 +1,17 @@
 resource "grafana_folder" "Production" {
+  provider = grafana.second
+
   title = "Production"
 }
 
 resource "grafana_folder" "Staging" {
+  provider = grafana.second
+
   title = "Staging"
 }
 
 resource "grafana_folder" "Dev" {
+  provider = grafana.second
+
   title = "Dev"
 }
